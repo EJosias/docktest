72f7d0ae6208b807ddee7ebb549ec96d732d2c74
Evandro Josias <88682660+EvandroJosias@users.noreply.github.com>
2022-02-11 12:56:27 -0300

    Update email.py

--- home/email.py
+++ home/email.py
@@ -1,1 +1,25 @@
-##
+from django.core.mail import send_mail
+from admin.settings import EMAIL_HOST_USER
+
+
+class EMail():
+
+    def setTo(Self, pTo):
+        Self.pTo = pTo
+
+    def setNumber(Self, pRFCNumber):
+        Self.pRFCNumber = pRFCNumber
+
+    def setTypeComplice(Self, pType):
+        if(pType == 1):
+            Self.pSub = "Not Complice RFC Number "+Self.pRFCNumber+"."
+            Self.pMsg = "You are recive this email because RFC Number " + \
+                Self.pRFCNumber+" should are closed."
+        elif(pType == 2):
+            Self.pSub = "Not Complice RFC Number "+Self.pRFCNumber+"."
+            Self.pMsg = "You are recive this email because RFC Number " + \
+                Self.pRFCNumber+" should are closed."
+
+    def sendMail(Self, pDest):
+        send_mail(Self.pSub, Self.pMsg, EMAIL_HOST_USER,
+                  list(pDest), fail_silently=False)
